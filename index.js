4f69bbba8589ce42abe2760bee976189b12e9f21
Maticostanzadev <maticostanzadev@gmail.com>
2023-01-17 06:11:35 -0300

    Fix deploy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./src/app.js');
 require('dotenv').config();
-const { PORT } = process.env;
+const { PORT } = require("./config.js")
 
 server.listen(PORT, () => {
   console.log(`%s listening at ${PORT} `); // eslint-disable-line no-console
